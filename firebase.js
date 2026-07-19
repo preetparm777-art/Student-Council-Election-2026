d7b5fc9df6143d1d3fcb4e05ac370605ad89d38c
preetparm777-art <preetparm777@gmail.com>
2026-07-19 23:46:19 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0B8vJTVD5Y05wENp6zMRYfcryBT_pS_A",
+  authDomain: "student-election-2026-fc050.firebaseapp.com",
+  projectId: "student-election-2026-fc050",
+  storageBucket: "student-election-2026-fc050.firebasestorage.app",
+  messagingSenderId: "1090530844419",
+  appId: "1:1090530844419:web:e793ec58af8550f3719336"
+};
